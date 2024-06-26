eb8ec4aae9dc11f7a6d5ab261473cc96143e3460
aditikalvium <aditi.vashishtha@kalvium.community>
2024-06-26 05:36:50 +0530

    jwt added

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,10 @@
 const config = {
   mongoURI:
     "mongodb+srv://KALVIUM_ASAP:ADITI123@kalvium.fmgzr6m.mongodb.net/test",
+  ACCESS_TOKEN_SECRET:
+    "41b26d536d7c51395e034f168197959ff224eb60f052e96c325a1d07261592bd",
+  REFRESH_TOKEN_SECRET:
+    "d0d896d87d7a79d1edeba57b7fc9d98bfff51e25b76a888c026c95b04f708d5d",
 };
 
 module.exports = config;
