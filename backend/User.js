b2a6b48916f40bda5a195b6bc0f15af1a2acc4da
aditikalvium <aditi.vashishtha@kalvium.community>
2024-06-29 12:00:21 +0530

    Login & Logout

--- backend/User.js
+++ backend/User.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
+const Joi = require("joi");
+
+const userValidationSchema = Joi.object({
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string()
+    .pattern(new RegExp("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@#$]).{8,}$"))
+    .required()
+    .messages({
+      "string.pattern.base":
+        "Password must be at least 8 characters long, include at least one letter, one number, and one special character (@, #, $).",
+    }),
+});
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
@@ -24,6 +36,18 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
-const UserModel = mongoose.model("hello", userSchema);
+userSchema.pre("validate", function (next) {
+  const { error } = userValidationSchema.validate({
+    username: this.username,
+    password: this.password,
+  });
+  if (error) {
+    next(new Error(error.details[0].message));
+  } else {
+    next();
+  }
+});
+
+const UserModel = mongoose.model("User", userSchema);
 
 module.exports = UserModel;
